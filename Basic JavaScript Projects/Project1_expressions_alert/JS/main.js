4e27d0c1b8d746c468adf8e617509e9ab4c93b7e
laura02040 <131707496+laura02040@users.noreply.github.com>
2024-03-11 21:00:12 +0000

    Basic JavaScript project
    update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+window.alert("Hello world!"); //windows alert method
+document.write("Lisa told bart, \"Stop, \"be calm\"<br>\"relax"); //document write method
+var Sent1= "Beginning"; //creating two variables
+var Sent2= "End";
+document.write(Sent + Sent2); //display variables
+
+document.write(3 * 3);   //expression
