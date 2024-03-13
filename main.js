2b61b85b4e988de93d21c9d544fa1f8e3889e810
laura02040 <131707496+laura02040@users.noreply.github.com>
2024-03-13 17:18:39 +0000

    up to date

---
+++ main.js
@@ -0,0 +1,5 @@
+function My_First_Function() { 
+    var sentence ="This is the beginning of the string"; //creating variable
+    sentence +="And this is end of the string"; //+=operator to add to the variable
+    document.getElementById("Concatenate").innerHTML=sentence; //using document.getElementById()
+}
